985b16d7e264bf88914cfd77cbde95a72da1f7fe
Andrii Andreichenko <87246480+shrewddd@users.noreply.github.com>
2026-08-06 12:15:19 +0200

    refactor: hpp file

--- db.cpp
+++ db.cpp
@@ -1,52 +1,41 @@
-#include <sqlite3.h>
+#include "db.hpp"
 #include <stdexcept>
-#include <string>
 
-class Database final {
-public:
-  explicit Database(const std::string &filename): Database(filename.c_str()) {}
+Database::Database(const std::string &filename): Database(filename.c_str()) {}
 
-  explicit Database(const char* filename) {
-    if (int rc = sqlite3_open(filename, &_db); rc != SQLITE_OK) {
-      std::string msg = sqlite3_errmsg(_db);
-      sqlite3_close_v2(_db);
-      throw std::runtime_error("sqlite3_open failed: " + msg);
-    }
+Database::Database(const char* filename) {
+  if (int rc = sqlite3_open(filename, &_db); rc != SQLITE_OK) {
+    std::string msg = sqlite3_errmsg(_db);
+    sqlite3_close_v2(_db);
+    throw std::runtime_error("sqlite3_open failed: " + msg);
   }
+}
 
-  Database(const Database&) = delete; 
+Database::Database(Database&& other) noexcept : _db(other._db) {
+  other._db = nullptr;
+}
 
-  Database& operator=(const Database&) = delete;
-
-  Database(Database&& other) noexcept : _db(other._db) {
+Database& Database::operator=(Database&& other) noexcept {
+  if (this != &other) {
+    sqlite3_close_v2(_db);
+    _db = other._db;
     other._db = nullptr;
   }
+  return *this;
+}
 
-  Database& operator=(Database&& other) noexcept {
-    if (this != &other) {
-      sqlite3_close_v2(_db);
-      _db = other._db;
-      other._db = nullptr;
-    }
-    return *this;
+Database::~Database() { 
+  sqlite3_close_v2(_db);
+}
+
+void Database::query(const std::string &sql) { query(sql.c_str()); }
+
+void Database::query(const char* sql) {
+  char *errMsg = nullptr;
+
+  if (const int rc = sqlite3_exec(_db, sql, nullptr, nullptr, &errMsg); rc != SQLITE_OK) {
+    const std::string msg = errMsg ? errMsg : "unknown error";
+    sqlite3_free(errMsg);
+    throw std::runtime_error("sqlite3_exec failed: " + msg);
   }
-
-  ~Database() { 
-    sqlite3_close_v2(_db);
-  }
-
-  void query(const std::string &sql) { query(sql.c_str()); }
-
-  void query(const char* sql) {
-    char *errMsg = nullptr;
-
-    if (const int rc = sqlite3_exec(_db, sql, nullptr, nullptr, &errMsg); rc != SQLITE_OK) {
-      const std::string msg = errMsg ? errMsg : "unknown error";
-      sqlite3_free(errMsg);
-      throw std::runtime_error("sqlite3_exec failed: " + msg);
-    }
-  }
-
-private:
-  sqlite3 *_db = nullptr;
-};
+}
--- db.hpp
+++ db.hpp
@@ -1,12 +1,19 @@
+#pragma once
 #include <sqlite3.h>
 #include <string>
 
-class Database {
+class Database final {
 public:
-  Database(const std::string &filename);
+  explicit Database(const std::string &filename);
+  explicit Database(const char* filename);
+  Database(const Database&) = delete;
+  Database& operator=(const Database&) = delete;
+  Database(Database&& other) noexcept;
+  Database& operator=(Database&& other) noexcept;
   ~Database();
-  void query(std::string &query);
+  void query(const std::string &sql);
+  void query(const char* sql);
 
 private:
-  sqlite3 *_db;
+  sqlite3 *_db = nullptr;
 };
